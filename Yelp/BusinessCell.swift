b89f11c045337f740c26431f0416252c97bade2c
Lee Edwards <lee.edwards@teespring.com>
2016-02-06 19:29:40 -0800

    Homework Part 1

---
+++ Yelp/BusinessCell.swift
@@ -0,0 +1,39 @@
+//
+//  BusinessCell.swift
+//  Yelp
+//
+//  Created by Lee Edwards on 2/6/16.
+//  Copyright © 2016 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+
+class BusinessCell: UITableViewCell {
+    @IBOutlet weak var thumbImageView: UIImageView!
+    @IBOutlet weak var ratingLabel: UIImageView!
+    @IBOutlet weak var reviewsCountLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var categoriesLabel: UILabel!
+    @IBOutlet weak var distanceLabel: UILabel!
+    
+    var business: Business! {
+        didSet {
+            nameLabel.text = business.name
+            thumbImageView.setImageWithURL(business.imageURL!)
+            ratingLabel.setImageWithURL(business.ratingImageURL!)
+            reviewsCountLabel.text = "\(business.reviewCount!) Reviews"
+            addressLabel.text = business.address
+            categoriesLabel.text = business.categories
+            distanceLabel.text = business.distance
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        thumbImageView.layer.cornerRadius = 5
+        thumbImageView.clipsToBounds = true
+    }
+
+}
